cba11b5e7eb38ebfd80d0593506ece57ea480ca8
victim0596 <81644467+victim0596@users.noreply.github.com>
2022-01-06 17:34:57 +0100

    Added method entropyTips for give a message to user

--- PasswordManagerWPF/Classes/EntropyCalc.cs
+++ PasswordManagerWPF/Classes/EntropyCalc.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace PasswordManagerWPF.Classes
 {
     internal class EntropyCalc
     {
+        public EntropyCalc()
+        {
+            globalVar.lowerCase = 0;
+            globalVar.upperCase = 0;
+            globalVar.digits = 0;
+            globalVar.simbol = 0;
+        }
         public string entropy(double length)
         {
             double entropy = 0;
@@ -20,6 +27,45 @@ namespace PasswordManagerWPF.Classes
             if (globalVar.simbol > 0) poolSize = poolSize + 32;
             entropy = length * Math.Log2(poolSize);
             return entropy.ToString("0.00");
+        }
+        public void entropyTips(string entropyValue, Label entropyLabel)
+        {
+            string message = "";
+            double entropyBit = Double.Parse(entropyValue);
+            switch (entropyBit)
+            {
+                case < 17:
+                    {
+                        message = "Very weak password, generate one more with more option or change length";
+                        entropyLabel.Foreground = new SolidColorBrush(Colors.Red);
+                        break;
+                    }
+                case double n when (n >= 17 && n <= 36):
+                    {
+                        message = "Weak password, generate one more with more option or change length";
+                        entropyLabel.Foreground = new SolidColorBrush(Colors.Orange);
+                        break;
+                    }
+                case double n when (n > 36 && n <= 60):
+                    {
+                        message = "Strong password, but I recommend that you generate one with more than 60 bits of entropy";
+                        entropyLabel.Foreground = new SolidColorBrush(Colors.Yellow);
+                        break;
+                    }
+                case double n when (n > 60 && n <= 124):
+                    {
+                        message = "Very strong password, very well";
+                        entropyLabel.Foreground = new SolidColorBrush(Colors.GreenYellow);
+                        break;
+                    }
+                case > 125:
+                    {
+                        message = "Password impossible to crack, even with all the computers in the universe together";
+                        entropyLabel.Foreground = new SolidColorBrush(Colors.Green);
+                        break;
+                    }
+            }
+            entropyLabel.Content = message;
         }
     }
 }
