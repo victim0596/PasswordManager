f023ce263a7c43429d6d65d78adf669ee13c3823
victim0596 <81644467+victim0596@users.noreply.github.com>
2022-01-06 17:36:36 +0100

    Added class for password manager object

---
+++ PasswordManagerWPF/Classes/passwordManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManagerWPF.Classes
+{
+    public class passwordManager
+    {
+        public string appName { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+
+    }
+}
