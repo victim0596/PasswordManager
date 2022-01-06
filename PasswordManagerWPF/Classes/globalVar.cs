482c2bb71200a35083a36b84e607d07e31a0ed54
victim0596 <81644467+victim0596@users.noreply.github.com>
2022-01-06 17:35:18 +0100

    Added variable for check if user is already logged in

--- PasswordManagerWPF/Classes/globalVar.cs
+++ PasswordManagerWPF/Classes/globalVar.cs
@@ -4,13 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace PasswordManagerWPF.Classes
 {
     static class globalVar
     {
+        //for entropy calculation
         public static int lowerCase;
         public static int upperCase;
         public static int digits;
         public static int simbol;
+        //for checking if user is logged
+        public static bool userLogged;
     }
 }
