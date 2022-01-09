61f13e3bdc8c36e769672eb1d80fe28f4bdd3e31
victim0596 <81644467+victim0596@users.noreply.github.com>
2022-01-09 20:52:32 +0100

    Added configuration btn and option

---
+++ PasswordManagerWPF/Classes/Configuration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManagerWPF.Classes
+{
+    public class Configuration
+    {
+        public string resolutionScreen { get; set; }
+        public string language { get; set; }
+        public string[] backgroundColor { get; set; }
+        public string[] fontColor { get; set; }
+
+        public void loadConfig(MainWindow mainWindow)
+        {
+            //load resolution
+            Resolution res = new Resolution();
+            res.loadRes(mainWindow);
+
+            //load Language
+            Languages lang = new Languages();
+            lang.loadLanguages();
+        }
+    }
+}
---
+++ PasswordManagerWPF/Configurator.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using PasswordManagerWPF.Classes;
+using PasswordManagerWPF.GVariable;
+
+namespace PasswordManagerWPF
+{
+    /// <summary>
+    /// Logica di interazione per Configurator.xaml
+    /// </summary>
+    public partial class Configurator : Window
+    {
+        public Configurator()
+        {
+            InitializeComponent();
+            resolution.Text = globalVar.resolution;
+            language.Text = globalVar.language;
+        }
+
+        public void colorPaletteBackground(object sender, RoutedEventArgs e)
+        {
+            ColorChange colorChange = new ColorChange();
+            colorChange.ShowDialog();
+        }
+        public void colorPaletteFont(object sender, RoutedEventArgs e)
+        {
+
+        }
+        public void saveBtn(object sender, RoutedEventArgs e)
+        {
+            Resolution res = new Resolution();
+            Languages lang = new Languages();
+            try
+            {
+                res.saveRes(resolution.Text);
+                lang.saveLanguages(language.Text);
+                MessageBox.Show(LangString.savedConfig);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
