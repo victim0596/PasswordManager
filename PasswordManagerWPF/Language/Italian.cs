82cfbf2ced254f156fd6999d3a4c73d98af0364e
victim0596 <81644467+victim0596@users.noreply.github.com>
2022-01-09 20:55:03 +0100

    Added Italian translation

---
+++ PasswordManagerWPF/Language/Italian.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PasswordManagerWPF.GVariable;
+
+namespace PasswordManagerWPF.Language
+{
+    internal class Italian
+    {
+        public void setItaliano()
+        {
+            //home
+            LangString.createPswBtn = "Crea Password";
+            LangString.savedPswBtn = "Password Salvate";
+            //configurator
+            LangString.resolution = "Risoluzione";
+            LangString.language = "Lingua";
+            LangString.bgColor = "Colore di sfondo";
+            LangString.fontColor = "Colore caratteri";
+            LangString.savedConfig = "Configurazione salvata, ricarica l'app per vedere le modifiche";
+            //btn
+            LangString.saveBtn = "Salva";
+            //create psw
+            LangString.number = "Numeri";
+            LangString.character = "Caratteri";
+            LangString.simbol = "Simboli";
+            LangString.length = "Lunghezza";
+            LangString.genPsw = "Password generata:";
+            LangString.entropyBit = "Entropia bit:";
+            LangString.entropyMessage = "";
+            LangString.genPswBtn = "Genera";
+            LangString.pswCopied = "Password copiata";
+            LangString.pswGenMes = "Password generata";
+            //entropy tips
+            LangString.tip1 = "Password molto debole, generane un'altra con più opzioni o cambia lunghezza";
+            LangString.tip2 = "Password debole, generane un'altra con più opzioni o cambia lunghezza";
+            LangString.tip3 = "Password complessa, ma ti consiglio di generarne una con più di 60 bit di entropia";
+            LangString.tip4 = "Password molto forte, molto bene";
+            LangString.tip5 = "Password impossibile da decifrare, anche con tutti i computer dell'universo insieme";
+            //login
+            LangString.register = "REGISTRATI";
+            LangString.userCreated = "Utente creato!";
+            //saved psw
+            LangString.pswAdded = "Password aggiunta!";
+            LangString.appName = "Nome app";
+            LangString.addBtn = "AGGIUNGI";
+        }
+    }
+}
