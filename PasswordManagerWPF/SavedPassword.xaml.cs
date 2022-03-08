b08c91a99972fbf358b198ab5cddf90967525eab
victim0596 <81644467+victim0596@users.noreply.github.com>
2022-03-08 17:30:38 +0100

    Added edit, show and copy btn function to grid

--- PasswordManagerWPF/SavedPassword.xaml.cs
+++ PasswordManagerWPF/SavedPassword.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -28,7 +31,8 @@ namespace PasswordManagerWPF
             try
             {
                 dbClass db = new dbClass();
-                savedPswDB.ItemsSource = db.loadSavedPassword();
+                globalVar.listPsw = db.loadSavedPassword();
+                savedPswDB.ItemsSource = globalVar.listPsw;
                 ColorChanger colorChanger = new ColorChanger();
                 colorChanger.applyFontSavPsw(this);
                 fontSize fontSize = new fontSize();
@@ -68,5 +72,64 @@ namespace PasswordManagerWPF
             MessageBox.Show("Logged Out");
             this.NavigationService.Navigate(new HomePage());
         }
+        //copy function
+        private void CopyPsw(int index)
+        {
+            Clipboard.SetText(globalVar.listPsw[index].password);
+            MessageBox.Show(LangString.pswCopied);
+        }
+
+
+        private void showPsw(int rows, int column)
+        {
+
+            var cellInfo = savedPswDB.SelectedCells[column - 1];
+            var content = (cellInfo.Column.GetCellContent(cellInfo.Item) as TextBlock).Text;
+
+            if (content == string.Empty) (cellInfo.Column.GetCellContent(cellInfo.Item) as TextBlock).Text = globalVar.listPsw[rows].password;
+            else (cellInfo.Column.GetCellContent(cellInfo.Item) as TextBlock).Text = string.Empty;
+        }
+
+        private void savedPswDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            int selectedIndex = savedPswDB.SelectedIndex;
+            int selectedColum = savedPswDB.CurrentColumn.DisplayIndex;
+            switch (selectedColum)
+            {
+                case 3:
+                    {
+                        showPsw(selectedIndex, selectedColum);
+                        break;
+                    }
+                case 4:
+                    {
+                        EditRow(selectedIndex);
+                        break;
+                    }
+                case 5:
+                    {
+                        CopyPsw(selectedIndex);
+                        break;
+                    }
+            }
+        }
+
+        private void EditRow(int selectedIndex)
+        {
+            //info per appname
+            var cellInfoAppName = savedPswDB.SelectedCells[0];
+            var contentAppName = (cellInfoAppName.Column.GetCellContent(cellInfoAppName.Item) as TextBlock).Text;
+            //info per username
+            var cellInfoUsername = savedPswDB.SelectedCells[1];
+            var contentUsername = (cellInfoUsername.Column.GetCellContent(cellInfoUsername.Item) as TextBlock).Text;
+
+            globalVar.appNameDetail = contentAppName;
+            globalVar.passwordDetail = globalVar.listPsw[selectedIndex].password;
+            globalVar.usernameDetail = contentUsername;
+            globalVar.idRowDetail = globalVar.listPsw[selectedIndex].id;
+            EditDetail editDetail = new EditDetail();
+            editDetail.ShowDialog();
+        }
+
     }
 }
