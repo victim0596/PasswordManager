7ed33f5f120bd4864cfd8667f704650b9d116675
victim0596 <81644467+victim0596@users.noreply.github.com>
2022-03-11 16:05:03 +0100

    Added entity framework core

---
+++ PasswordManagerWPF/Domain/PswManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManagerWPF.Domain
+{
+    public class PswManager
+    {
+        public int ID { get; set; }
+        public string APPNAME { get; set; }
+        public string USERNAME { get; set; }
+        public string PASSWORD { get; set; }
+    }
+}
---
+++ PasswordManagerWPF/Domain/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManagerWPF.Domain
+{
+    public class User
+    {
+        public string USERNAME { get; set; }
+        public string PASSWORD { get; set; }
+    }
+}
